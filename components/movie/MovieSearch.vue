1c6110b58316c7f14c2408a6fcb419c03f6e6f6e
Johnny Santos <fagundesjohnny@gmail.com>
2023-10-11 13:45:23 -0300

    feat: Working with async data in Nuxt

---
+++ components/movie/MovieSearch.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue'
+
+const query = ref("batman")
+const movies = ref([])
+
+async function search() {
+  const {Search} = await $fetch(`http://www.omdbapi.com/?apiKey=8e3f600b&s=${query.value}`)
+  movies.value = Search
+}
+search()
+</script>
+
+<template>
+  <div>
+    <form @submit.prevent="search">
+      <input type="text" v-model="query">
+      <button>Search</button>
+    </form>
+    <ul style="display: flex; flex-wrap: wrap; gap: 10px; list-style: none">
+      <li v-for="movie in movies" :key="movie.imdbID">
+        <NuxtLink :to="{ name: 'movies-id', params: { id: movie.imdbID } }">
+          <img :src="movie.Poster" :alt="movie.title"/>
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -1,8 +1,31 @@
-<script lang="ts" setup></script>
+<script setup>
+const route = useRoute()
+
+const { data, error } = await useFetch(
+    `https://httpbin.org/status/500`,
+    {
+      pick: ["Plot", "Title", "Error"],
+      key: `/movies/${route.params.id}`
+    })
+if (error.value) {
+  showError({statusCode: 500, statusMessage: "Ops"})
+}
+if (data?.value?.Error === "Incorrect IMDb ID.") {
+  showError({statusCode: 404, statusMessage: "Page Not Found"})
+}
+
+// const { data } = useAsyncData(`/movie/${route.params.id}`,() => {
+//   return $fetch(`http://www.omdbapi.com/?apiKey=8e3f600b&i=${route.params.id}`)
+// }, {
+//   pick: ["Plot", "Title"],
+// })
+</script>
 
 <template>
   <div>
-    <h1>{{$route.params.id}}</h1>
+    <pre>
+      {{ data }}
+    </pre>
   </div>
 </template>
 
